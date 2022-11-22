849801e2b50728d656dd944dd1d1fd14e8aa4f8e
Thiago Neres <thiagoneres12@gmail.com>
2022-11-22 18:10:07 -0300

    Testing

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 # Create your models here.
 class Books(models.Model):
     bookID= models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title= models.CharField(max_length=255)
+    title= models.CharField(max_length=200)
     author= models.CharField(max_length=255)
     releaseYear= models.IntegerField()
     state = models.CharField(max_length=50)
